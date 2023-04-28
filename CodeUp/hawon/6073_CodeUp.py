ee21740db8e2975e784523c6410015c8b1d26e37
리보 <112055211+trumpetflor@users.noreply.github.com>
2023-04-28 16:47:31 +0900

    4회차 | [기초-반복실행구조] 6071 - 6076

---
+++ CodeUp/hawon/6073_CodeUp.py
@@ -0,0 +1,22 @@
+﻿'''
+6073번
+https://codeup.kr/problem.php?id=6073
+
+@문제
+정수 1개 입력받아 카운트다운 출력하기
+
+@입력
+정수 1개가 입력된다.
+(1 ~ 100)
+
+@출력
+1만큼씩 줄이면서 카운트다운 수가 0이 될 때까지 한 줄에 1개씩 출력한다.
+'''
+num = int(input())
+
+while num >= 0:
+  num = num - 1 if num != 0 else num
+  print(num)  #num을 출력한다
+  if num == 0:
+    break
+
---
+++ CodeUp/hawon/6074_CodeUp.py
@@ -0,0 +1,23 @@
+﻿'''
+6074번
+https://codeup.kr/problem.php?id=6074
+
+@문제
+영문 소문자(a ~ z) 1개가 입력되었을 때,
+a부터 그 문자까지의 알파벳을 순서대로 출력해보자.
+
+@입력
+정수 1개가 입력된다.
+(1 ~ 100)
+
+@출력
+a부터 입력한 문자까지 순서대로 공백을 두고 한 줄로 출력한다.
+'''
+
+end_cha = ord(input())
+start = ord('a') #시작문자자
+
+while start <= end_cha:
+  print(chr(start), end=' ')  # 유니코드 문자: chr(정수값)
+  start += 1
+
