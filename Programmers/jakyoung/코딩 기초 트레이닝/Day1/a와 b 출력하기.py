640449a2ccdac14b46b3fc3d42ee9f7a21778da5
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-05-10 11:51:45 +0900

    7회차 | [Day1] a와 b 출력하기

---
+++ Programmers/jakyoung/코딩 기초 트레이닝/Day1/a와 b 출력하기.py
@@ -0,0 +1,9 @@
+'''
+[출력] a와 b 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181951
+'''
+
+a, b = map(int, input().strip().split(' ')) # strip() : 문자열 및 공백 제거
+# print(a + b) # a와 b를 더한 값이 출력
+print("a =", a)
+print("b =", b)
