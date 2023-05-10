bb0b28db6bf67b662c2642b9322732056ff6ab7c
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-05-10 11:59:24 +0900

    7회차 | [Day2] 문자열 붙여서 출력하기

---
+++ Programmers/jakyoung/코딩 기초 트레이닝/Day2/문자열 붙여서 출력하기.py
@@ -0,0 +1,7 @@
+'''
+[출력, 연산] 문자열 붙여서 출력하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181946
+'''
+
+str1, str2 = input().strip().split(' ')
+print(str1, str2, sep='') # 문자열 사이에 공백 없이 출력
