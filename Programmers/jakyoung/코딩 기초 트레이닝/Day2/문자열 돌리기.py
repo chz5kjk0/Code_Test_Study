a4af1d50170cc8cdbf8f13a909eebbbc66deb0b2
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-05-10 12:01:09 +0900

    7회차 | [Day2] 문자열 돌리기

---
+++ Programmers/jakyoung/코딩 기초 트레이닝/Day2/문자열 돌리기.py
@@ -0,0 +1,21 @@
+'''
+[출력, 연산] 문자열 돌리기
+https://school.programmers.co.kr/learn/courses/30/lessons/181945
+'''
+
+str = input()
+
+for i in str :
+    print(i)
+
+'''
+# 다른 풀이
+chrList = []
+for i in str :
+    chrList.append(i)
+
+chrList = list(str)
+for i in len(chrList) :
+    print(chrList[i])
+
+'''
