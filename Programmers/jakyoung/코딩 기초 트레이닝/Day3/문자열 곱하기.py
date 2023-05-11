6e86a85036c6a026091af57bc778f166acf96f40
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-05-11 12:20:55 +0900

    7회차 | [Day3] 문자열 곱하기

---
+++ Programmers/jakyoung/코딩 기초 트레이닝/Day3/문자열 곱하기.py
@@ -0,0 +1,11 @@
+'''
+[연산] 문자열 곱하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181940
+'''
+
+def solution(my_string, k):
+    answer = ''
+    
+    answer += my_string * k
+    
+    return answer
