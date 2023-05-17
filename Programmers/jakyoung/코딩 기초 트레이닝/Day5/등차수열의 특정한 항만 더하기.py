9579faa081a89c4acd0ffa8a825e5e2f79002346
chz5kjk0 <118323405+chz5kjk0@users.noreply.github.com>
2023-05-17 15:32:33 +0900

    7회차 | [Day5] 등차수열의 특정한 항만 더하기

---
+++ Programmers/jakyoung/코딩 기초 트레이닝/Day5/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,13 @@
+'''
+[조건문] 등차수열의 특정한 항만 더하기
+https://school.programmers.co.kr/learn/courses/30/lessons/181931
+'''
+
+def solution(a, d, included):
+    answer = 0
+    
+    for i in range (len(included)) :
+        if included[i] :
+            answer += a + d * i
+    
+    return answer
